e51e4f1b31bd42abba4affba9e183c4e3de4cf14
Kacper Ludwiczak <casper.ludwiczak@gmail.com>
2025-09-05 10:18:45 +0200

    Add DOM Interaction

---
+++ 2. DOM Interaction/First Goals Code/app.js
@@ -0,0 +1,21 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoalA: 'Start Learn',
+      courseGoalB: 'Finish Learn',
+      vueLink: 'https://vuejs.org/'
+    };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
+    }
+  }
+});
+
+app.mount('#user-goal');
